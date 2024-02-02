8352efb8e8884b83a36d077606d6e3ffdcd36fff
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-02-02 12:15:20 -0600

    Create template for 2 columns

---
+++ template-2-col-layout.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="a-style.css">
+  <link rel="stylesheet" href="bootstrap-m-p.css">
+
+  <title>********** | Andrew Blythe</title>
+  <!---------------------------------
+    Name: Andrew Blythe
+    Netid: cab1361
+    Date: *******
+
+    Hw***
+   --------------------------------->
+  <style>
+    #main-form table tr td:first-child {
+      text-align: left;
+    }
+    #results-table tbody tr td:nth-child(2) {
+      /* target specifically the 2nd column */
+      text-align: center;
+      padding: 0 !important;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="a-container">
+    <!-- col-container = grid -->
+    <SECTION class="header-left-right-container w3-card">
+
+      <!-- header -->
+      <section class="header a-center">
+        <h2 class="w3-large mt-0 mb-1">Homework ***************</h2>
+        <!-- <h1 class="w3-xxlarge mt-0" style="margin-bottom: 1rem">title</h1> -->
+      </section>
+
+      <!-- left -->
+      <section class="left a-center">      
+        <span>&nbsp;</span>
+      </section>
+
+      <!-- right -->
+      <section id="results" class="right">
+        <span>&nbsp;</span>
+      </section>
+
+    </SECTION>
+  </div>
+</body>
+
+</html>
