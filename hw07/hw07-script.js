cbccd6f633aa69742b45a5d9a685b928419d56b5
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-04-19 11:55:27 -0500

    all outputs are undefined... also sync vs async, need async

--- hw07/hw07-script.js
+++ hw07/hw07-script.js
@@ -9,7 +9,7 @@ function handleInput() {
     let continents = document.getElementsByName("continent");
     let foundChecked = false;
     let results = document.getElementById("results");
-    let output = null;
+    let output = [];
     let i = 0;
 
     // Determine which radio button is selected
@@ -18,12 +18,12 @@ function handleInput() {
         if (continents[i].checked) 
         {
             foundChecked = true;
-            output = loadAndFilterXML(continents[i].value); //get the data from XML file
+            output += loadAndFilterXML(continents[i].value); //get the data from XML file
             break;
         }
     }
 
-    if (!foundChecked || output.length === 0)  //didn't find any matches the file
+    if (!foundChecked)  //didn't find any matches the file
     {
         let feedback = document.createElement("p");
         feedback.className = "warning-text w3-medium";
@@ -36,6 +36,25 @@ function handleInput() {
         let feedback = document.createElement("p");
         feedback.className = "warning-text w3-medium";
         feedback.innerHTML = "Error getting results from the file.";
+
+        results.innerHTML = feedback.outerHTML;
+    }
+    else if (foundChecked) //there was a match
+    {
+        //print the results to a table
+        let table = document.createElement("table");
+        table.className = "bold-borders-table";
+        table.innerHTML =   //begin table + header row
+            "<thead><tr><th>Name</th><th>Population</th><th>Area</th></tr></thead><tbody>";
+        
+        for (let i = 0; i < output.length; i++) 
+        {
+            table.innerHTML += 
+                "<tr><td>" + output[i].name + "</td><td>" + output[i].population + "</td><td>" + output[i].area + "</td></tr>";
+        }
+
+        table.innerHTML += "</tbody>";  //end table
+        results.innerHTML = table.outerHTML;    //finally, print out to results section
     }
 
 }
@@ -68,14 +87,16 @@ function loadAndFilterXML(selectedContinent) {
                     let population = countries[i].getElementsByTagName("pop")[0].textContent;
                     let area = countries[i].getElementsByTagName("area")[0].textContent;
 
-                    output += { name: name, population: population, area: area };
+                    output.push({ name: name, population: population, area: area });
                 }
             }
-            return output;
+            // Invoke the callback function with the output array
+            callback(output);
 
         }
         else {
-            return false;
+            // If an error occurred, invoke the callback with false
+            callback(false);
         }
     };
 
