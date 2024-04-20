4c1e70a192f59fb1fb660c30822ea7bb96cdaded
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-04-20 15:28:01 -0500

    redo all - working now

--- hw07/hw07-script.js
+++ hw07/hw07-script.js
@@ -1,106 +1,53 @@
 "use strict";
 // Event listener for the search button
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("submit-form1").addEventListener("click", handleInput);
+    document.getElementById("submit-form1").addEventListener("click", clickHandler);
 });
 
-function handleInput() {
-    
-    let continents = document.getElementsByName("continent");
-    let foundChecked = false;
-    let results = document.getElementById("results");
-    let output = [];
-    let i = 0;
-
-    // Determine which radio button is selected
-    for (; i < continents.length; i++) 
-    {
-        if (continents[i].checked) 
-        {
-            foundChecked = true;
-            output += loadAndFilterXML(continents[i].value); //get the data from XML file
-            break;
-        }
-    }
-
-    if (!foundChecked)  //didn't find any matches the file
-    {
-        let feedback = document.createElement("p");
-        feedback.className = "warning-text w3-medium";
-        feedback.innerHTML = "No results found for " + continents[i].value + ".";
-
-        results.innerHTML = feedback.outerHTML;
-    }
-    else if (output === false)  //there was an error
-    {
-        let feedback = document.createElement("p");
-        feedback.className = "warning-text w3-medium";
-        feedback.innerHTML = "Error getting results from the file.";
-
-        results.innerHTML = feedback.outerHTML;
-    }
-    else if (foundChecked) //there was a match
-    {
-        //print the results to a table
-        let table = document.createElement("table");
-        table.className = "bold-borders-table";
-        table.innerHTML =   //begin table + header row
-            "<thead><tr><th>Name</th><th>Population</th><th>Area</th></tr></thead><tbody>";
-        
-        for (let i = 0; i < output.length; i++) 
-        {
-            table.innerHTML += 
-                "<tr><td>" + output[i].name + "</td><td>" + output[i].population + "</td><td>" + output[i].area + "</td></tr>";
-        }
-
-        table.innerHTML += "</tbody>";  //end table
-        results.innerHTML = table.outerHTML;    //finally, print out to results section
-    }
-
-}
-
-
-// Load and filter the XML content. Returns array of matching countries or false if error
-function loadAndFilterXML(selectedContinent) {
-    // Create a new XMLHttpRequest object
+function clickHandler() {
     let xhr = new XMLHttpRequest();
-
-    // Set up the request
-    xhr.open("GET", "un.xml", true);
-
-    xhr.onload = function () {
-        if (xhr.status === 200) 
-        {
-            let responseXML = xhr.responseXML;                            // Parse XML response
-            let countries = responseXML.getElementsByTagName("country");  // Get all countries
-            let output = [];                                              // To hold output
-
-            for (let i = 0; i < countries.length; i++) 
-            {
-                // countries[i]'s continent
-                let continent = countries[i].getElementsByTagName("continent")[0].textContent;
-
-                if (continent === selectedContinent) 
-                {
-                    // Get the country name, population, and area
-                    let name = countries[i].getElementsByTagName("name")[0].textContent;
-                    let population = countries[i].getElementsByTagName("pop")[0].textContent;
-                    let area = countries[i].getElementsByTagName("area")[0].textContent;
-
-                    output.push({ name: name, population: population, area: area });
-                }
-            }
-            // Invoke the callback function with the output array
-            callback(output);
-
-        }
-        else {
-            // If an error occurred, invoke the callback with false
-            callback(false);
-        }
-    };
-
-    // Send the request, onload above will catch the response
+    xhr.addEventListener("readystatechange", function() { showFile(xhr); }, false);
+    xhr.open("GET", "un.xml");
     xhr.send();
 }
 
+function getContinent() {
+    //get selected continent from form1 radio button choice
+    let form1 = document.forms["form1"];
+    let selectedContinent = form1["continent"].value;
+    return selectedContinent;
+}
+
+function showFile(xhr) 
+{
+    let results = document.getElementById("results");   //print to this element
+
+    try {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) 
+        {
+            let countries = xhr.responseXML.getElementsByTagName("country");
+            let selectedContinent = getContinent();
+
+            let table = document.createElement("table");
+            table.className = "bold-borders-table black-thead table-padding-3px a-right";
+            table.innerHTML = "<tr><th>Country</th><th>Population</th><th>Area</th></tr>";
+
+            for (let i = 0; i < countries.length; i++) 
+            {
+                let countryElement = countries[i];
+                let continent = countryElement.getElementsByTagName("continent")[0].textContent;
+                if (continent === selectedContinent) 
+                {
+                    let row = table.insertRow();
+                    row.innerHTML = "<td>" + countryElement.getElementsByTagName("name")[0].textContent + "</td>"
+                     + "<td>" + countryElement.getElementsByTagName("pop")[0].textContent + "</td>"
+                     + "<td>" + countryElement.getElementsByTagName("area")[0].textContent + "</td>";
+                }
+            }
+
+            results.innerHTML = table.outerHTML;
+        }
+    } catch (error) {
+        results.innerHTML = error.message;
+    }
+}
