030d8d0c9d8f261241b2af4e3478cf45d38a5b64
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-04-24 23:02:10 -0500

    working

--- hw08/hw08-script.js
+++ hw08/hw08-script.js
@@ -9,7 +9,7 @@ function clickHandler() {
 
     // get the continent from form1
     let form1 = document.forms["form1"];
-    let selectedContinent = form1["continent"].value;
+    var selectedContinent = form1["continent"].value;
 
     // try to filter input. If they send something dumb, it says "no results for..."
     if (!selectedContinent || selectedContinent == null || selectedContinent == "") 
@@ -21,54 +21,62 @@ function clickHandler() {
     }
     else
     {
-        let xhr = new XMLHttpRequest();
-        xhr.addEventListener("readystatechange", function() { showFile(xhr, selectedContinent); }, false);
-        xhr.open("GET", "un.xml");
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) 
+                {
+                    let countries = JSON.parse(xhr.responseText).countries;
+                    showCountries(countries, selectedContinent);
+                } 
+                else {
+                    results.textContent = "Error: " + xhr.status;
+                }
+            }
+        }, false);
+        xhr.open("GET", "un.json");
         xhr.send();
     }
-
-
 }
 
-function showFile(xhr, selectedContinent) 
+function showCountries(countries, selectedContinent) 
 {
     let results = document.getElementById("results");   //print to this element
 
-    try {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) 
+    try 
+    {
+        let table = document.createElement("table");
+        table.className = "bold-borders-table black-thead table-padding-4px table-th-center table-td-right table-1st-td-left";
+        table.innerHTML = "<tr><th>Country</th><th>Population</th><th>Area</th></tr>";
+
+        let found_a_match = false;
+
+        for (let country of countries)
         {
-            let countries = xhr.responseXML.getElementsByTagName("country");
-
-            let table = document.createElement("table");
-            table.className = "bold-borders-table black-thead table-padding-4px table-th-center table-td-right table-1st-td-left";
-            table.innerHTML = "<tr><th>Country</th><th>Population</th><th>Area</th></tr>";
-
-            let found_a_match = false;
-
-            for (let i = 0; i < countries.length; i++) 
+            if (country.continent === selectedContinent) 
             {
-                let countryElement = countries[i];
-                let continent = countryElement.getElementsByTagName("continent")[0].textContent;
-                if (continent === selectedContinent) 
-                {
-                    found_a_match = true;
-                    let row = table.insertRow();
-                    row.innerHTML = "<td>" + countryElement.getElementsByTagName("name")[0].textContent + "</td>"
-                     + "<td>" + countryElement.getElementsByTagName("pop")[0].textContent + "</td>"
-                     + "<td>" + countryElement.getElementsByTagName("area")[0].textContent + "</td>";
-                }
-            }
-            if (!found_a_match) {
-                let p = document.createElement("p");
-                p.className = "warning-text w3-medium";
-                p.innerHTML = "No matches found for continent of " + selectedContinent;
-                results.innerHTML = p.outerHTML;
-            }
-            else {
-                results.innerHTML = table.outerHTML;
+                found_a_match = true;
+                let row = table.insertRow();
+                row.innerHTML = "<td>" + country.name + "</td>" +
+                    "<td>" + country.pop + "</td>" +
+                    "<td>" + country.area + "</td>";
             }
         }
+        if (!found_a_match) {
+            let p = document.createElement("p");
+            p.className = "warning-text w3-medium";
+            p.innerHTML = "No matches found for continent of " + selectedContinent;
+            results.innerHTML = p.outerHTML;
+        }
+        else {
+            results.innerHTML = table.outerHTML;
+        }
+
     } catch (error) {
-        results.innerHTML = error.message;
+        let p = document.createElement("p");
+        p.className = "warning-text w3-medium";
+        p.innerHTML = "Unable to retrieve data: " + error.message;
+        results.innerHTML = "";
+        results.appendChild(p);
     }
 }
